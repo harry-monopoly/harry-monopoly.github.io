2e3059098a3f8f185fb585bc4f417af778e5eaf4
Stanislav Pomatsuev <pomatsuev@gmail.com>
2020-04-24 20:22:39 +1000

    pwa

---
+++ precache-manifest.6d5ac609c15dd28f19f2cd98cb5b1667.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fe27ee0f2ab561f012",
+    "url": "/css/app.9db081e5.css"
+  },
+  {
+    "revision": "e3291f33a1ec2425a17ac70be005d67b",
+    "url": "/fonts/10467.e3291f33.ttf"
+  },
+  {
+    "revision": "20118a32cdc94c9e886ddb41c8fe4111",
+    "url": "/img/1580653594_3-p-foni-s-teksturoi-pergamentnogo-lista-9.20118a32.jpg"
+  },
+  {
+    "revision": "2c0eed72e37e734bf147c049b24f53a2",
+    "url": "/img/card1.2c0eed72.png"
+  },
+  {
+    "revision": "c421e57fb171e14b635f52e7b169d114",
+    "url": "/img/card2.c421e57f.png"
+  },
+  {
+    "revision": "8dc2c40e00a8ed6dd67d0c44d9b4bd3a",
+    "url": "/img/card3.8dc2c40e.png"
+  },
+  {
+    "revision": "2f8f153e593a388ca57c84920a88c7c1",
+    "url": "/img/card4.2f8f153e.png"
+  },
+  {
+    "revision": "815cf357fd34df09f503986926a3f49d",
+    "url": "/img/card5.815cf357.png"
+  },
+  {
+    "revision": "36e30923d94b88ecebd7f35a151b99b7",
+    "url": "/img/card6.36e30923.png"
+  },
+  {
+    "revision": "52ab8b2e3517199d0a927c7bcd560216",
+    "url": "/img/card7.52ab8b2e.png"
+  },
+  {
+    "revision": "908067b300c784d11375b8abe07c1c6c",
+    "url": "/img/card8.908067b3.png"
+  },
+  {
+    "revision": "819735c6b7a2a56fc0827af3e4b6d37c",
+    "url": "/img/card9.819735c6.png"
+  },
+  {
+    "revision": "d9e2c53392b8e5481b19490fd9c7cfea",
+    "url": "/img/cardmagic.d9e2c533.png"
+  },
+  {
+    "revision": "2703327e1dfb501ea0e1b915a8085c16",
+    "url": "/img/hogwarts-logo-png-1.2703327e.png"
+  },
+  {
+    "revision": "7bbf6bb7c4363b096b3422799dbf23f6",
+    "url": "/img/ico128.png"
+  },
+  {
+    "revision": "8642cffe1656889dae99dd7a2026c351",
+    "url": "/img/ico144.png"
+  },
+  {
+    "revision": "caafa6d87b46b7edf88427809ee0aa04",
+    "url": "/img/ico152.png"
+  },
+  {
+    "revision": "5331ef1031562cab51d9c05d271cd059",
+    "url": "/img/ico192.png"
+  },
+  {
+    "revision": "2b0310c941f0d4deb0b065cc2dd939f3",
+    "url": "/img/ico384.png"
+  },
+  {
+    "revision": "93553439869330d9a8a341ed7338f29e",
+    "url": "/img/ico512.png"
+  },
+  {
+    "revision": "93553439869330d9a8a341ed7338f29e",
+    "url": "/img/ico521.png"
+  },
+  {
+    "revision": "3f69841ae03a3fb89e41b2a17e16c42a",
+    "url": "/img/ico72.png"
+  },
+  {
+    "revision": "01cc9d6244087c2cfed6bf946ac6353e",
+    "url": "/img/ico96.png"
+  },
+  {
+    "revision": "f6f30ce801253e6301633abaf801ec65",
+    "url": "/img/pennant.f6f30ce8.png"
+  },
+  {
+    "revision": "5b98cbdda4c9bd057ddc5f381dcfd361",
+    "url": "/img/text.5b98cbdd.png"
+  },
+  {
+    "revision": "7cc8ab48ad6e9041637a8a48d52378fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34fe27ee0f2ab561f012",
+    "url": "/js/app.d774b3cd.js"
+  },
+  {
+    "revision": "b5a59d727c191526be81",
+    "url": "/js/chunk-vendors.e036feab.js"
+  },
+  {
+    "revision": "1e97aee052d56075e54fec84e2632c6d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
